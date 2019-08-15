587b26ab716affbadccb959ddb5e3671ce147172
artem <gorbacartem90@gmail.com>
2019-08-15 16:39:08 +0300

    configure font

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import "./js/common"
 import "./css/main.css"
 import "./scss/main.scss"
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
