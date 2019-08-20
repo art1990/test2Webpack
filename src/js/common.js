576fdc213c30ee35506d00226b57cc6d56ccdcad
Artem <gorbacartem90@gmail.com>
2019-08-20 19:44:10 +0300

    finish project

--- src/js/common.js
+++ src/js/common.js
@@ -26,6 +26,6 @@ for (let i = 0; i < 4; i++) {
 let count = 2;
 window.setInterval(() => {
         count = count%4? count: 1;
-     document.body.style.backgroundImage = `url(/assets/img/slider/slide${count}.png)`;
+     document.body.style.backgroundImage = `linear-gradient(to top, rgba(23, 49, 83, 0.35), rgba(23, 49, 83, 0.35)),url(/assets/img/slider/slide${count}.png)`;
      count++
  }, 10000);
