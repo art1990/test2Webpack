498c1d31d218f359eacc95c194af595711e6379a
Artem <gorbacartem90@gmail.com>
2019-08-31 10:37:53 +0300

    Updates

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const PATH = {
     src: path.join(__dirname, "../src"),
-    dist: path.join(__dirname, "../docs"),
+    dist: path.join(__dirname, "../build"),
     assets: "assets/",
 };
 
