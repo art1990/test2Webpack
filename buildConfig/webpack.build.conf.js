a7988670375ae6ac7cd604f3b23688b1960fd906
Artem <gorbacartem90@gmail.com>
2019-08-31 10:40:57 +0300

    install gh-page

--- buildConfig/webpack.build.conf.js
+++ buildConfig/webpack.build.conf.js
@@ -2,10 +2,13 @@ const merge = require("webpack-merge");
 const baseWebpackConfig = require("./webpack.base.conf");
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
-    mode: "production",
-    plugins: []
-})
+  mode: "production",
+  plugins: [],
+  output: {
+    publicPath: "/test3/"
+  }
+});
 
 module.exports = new Promise((resolve, reject) => {
-    resolve(buildWebpackConfig)
-})
+  resolve(buildWebpackConfig);
+});
