d092f4348d97b2635715720645f09dde8937a94a
artem <gorbacartem90@gmail.com>
2019-09-04 15:21:37 +0300

    fixed white-space button and fixed header menu

--- src/js/common.js
+++ src/js/common.js
@@ -2,7 +2,9 @@ const btnFind = document.querySelector(".main__btn_find");
 const inputFind = document.querySelector(".main__input_find");
 const mainFind = document.querySelector(".main__form_find");
 const header = document.querySelector(".header");
+const headerMenu = document.querySelector(".header__menu");
 const btnCall = document.querySelector(".header__call");
+const headerMenuItem = header.querySelectorAll("li")
 
 mainFind.addEventListener("mouseover", ({ target }) => {
   inputFind.classList.remove("hidden");
@@ -41,3 +43,36 @@ window.onscroll = () => {
     btnCall.textContent = "CALL US";
   }
 };
+
+headerMenuItem.forEach(a => a.onclick = () => {
+  headerMenu.classList.add("menu-hidden")
+  setTimeout(() => {
+    headerMenu.classList.remove("menu-hidden")
+  }, 200)
+})
+
+computedStyle = function(vElement) {
+  return window.getComputedStyle
+         ?window.getComputedStyle(vElement,null)
+         :vElement.currentStyle; 
+}
+
+isVisible = function(vElement) {
+  return !(vElement.offsetWidth == 0 && vElement.offsetHeight == 0)
+         && computedStyle(vElement).visibility != "hidden";  
+}
+
+header.onmouseover = ({target}) => {
+  if(!target.classList.contains("header")) {return}
+  if (isVisible(headerMenu)) {
+    headerMenu.classList.add("menu-visible")
+    ;
+  }
+}
+
+header.onmouseout = () => {
+  if (isVisible(headerMenu)) {
+    headerMenu.classList.remove("menu-visible")
+    ;
+  }
+}
